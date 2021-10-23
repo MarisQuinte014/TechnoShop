c1f24230a66ef8b0106c5678bba1c40a7fe5d836
MarisQuinte014 <marisquinte@gmail.com>
2021-10-22 22:52:41 -0500

    Creación de proyecto

--- app/src/main/java/com/example/technoshop/Entities/Product.java
+++ app/src/main/java/com/example/technoshop/Entities/Product.java
@@ -7,4 +7,52 @@ public class Product {
     private int stock;
     private double price;
     private String category;
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
--- app/src/main/java/com/example/technoshop/MainActivity.java
+++ app/src/main/java/com/example/technoshop/MainActivity.java
@@ -6,14 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.technoshop.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mainBinding;
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = mainBinding.getRoot();
         setContentView(view);
+        db = FirebaseFirestore.getInstance();
     }
 }
