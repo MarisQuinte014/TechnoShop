68aa30637adbadaf56d2f90276c2648e340037e0
MarisQuinte014 <marisquinte@gmail.com>
2021-10-23 07:25:42 -0500

    por si las moscas

--- app/src/main/java/com/example/technoshop/MainActivity.java
+++ app/src/main/java/com/example/technoshop/MainActivity.java
@@ -4,13 +4,28 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.technoshop.Adapters.ProductAdapter;
+import com.example.technoshop.Entities.Product;
 import com.example.technoshop.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.remote.WatchChange;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mainBinding;
     private FirebaseFirestore db;
+    ArrayList<Product> productArrayList;
+    ProductAdapter productAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +33,28 @@ public class MainActivity extends AppCompatActivity {
         View view = mainBinding.getRoot();
         setContentView(view);
         db = FirebaseFirestore.getInstance();
+        productArrayList = new ArrayList<>();
+        productAdapter = new ProductAdapter(this,productArrayList);
+        mainBinding.rvProduct.setHasFixedSize(true);
+        mainBinding.rvProduct.setLayoutManager(new LinearLayoutManager(this));
+        mainBinding.rvProduct.setAdapter(productAdapter);
+    }
+
+    public void getProduct(){
+        db.collection("product").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null){
+                    Toast.makeText(getApplicationContext(), "Faltan datos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                for (DocumentChange dc : value.getDocumentChanges()){
+                    if (dc.getType() == DocumentChange.Type.ADDED){
+                        productArrayList.add(dc.getDocument().toObject(Product.class));
+                    }
+                }
+                productAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
