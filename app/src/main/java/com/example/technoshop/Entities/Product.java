23a43624f15f1e516fb4fd2312c11cd166141331
MarisQuinte014 <marisquinte@gmail.com>
2021-10-22 22:37:59 -0500

    Creacion del proyecto conectado con Firebase

---
+++ app/src/main/java/com/example/technoshop/Entities/Product.java
@@ -0,0 +1,10 @@
+package com.example.technoshop.Entities;
+
+public class Product {
+    private String productID;
+    private String name;
+    private String description;
+    private int stock;
+    private double price;
+    private String category;
+}
--- app/src/main/java/com/example/technoshop/MainActivity.java
+++ app/src/main/java/com/example/technoshop/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.technoshop;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.technoshop.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
     }
 }
